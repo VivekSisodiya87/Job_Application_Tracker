e8f026a9d4ba11bbe5f56a29b2d2ca58d3f9b700
Vivek Sisodiya <viveksisodiya@gmail.com>
2025-07-07 20:59:34 +0530

    second

---
+++ Job-Application-Tracker/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+
+const App = () => {
+  const isLoggedIn = !!localStorage.getItem('token');
+
+  return (
+    <Routes>
+      <Route path="/" element={isLoggedIn ? <Dashboard /> : <Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
+};
+
+export default App;
